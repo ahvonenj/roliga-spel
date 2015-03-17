df8af26b48f688fe3ffa13c710455c7280d5ae0a
J <jonah.ahvonen@gmail.com>
2015-03-17 13:10:55 +0200

    Add comments

--- programkod/index.js
+++ programkod/index.js
@@ -1,75 +1,72 @@
-var stadium;
-var renderare;
-var texter = [];
-var textIntervall = 2000;
-var textHastighet = 1;
-var timer;
+var stadium; // Stage
+var renderare; // Renderer
+var texter = []; // Array of visible texts
+var textIntervall = 2000; // Interval of new texts popping on the screen
+var textHastighet = 1; // Speed of texts
+var timer; // Text timer
 
-function initiera()
+function initiera() // Init the program / game
 {
-    stadium = new PIXI.Stage(0x66FF99);
+    stadium = new PIXI.Stage(0x66FF99); // Create stage
+    renderare = PIXI.autoDetectRenderer(800, 600); // Create renderer
  
-    // create a renderare instance.
-    renderare = PIXI.autoDetectRenderer(800, 600);
- 
-    // add the renderare view element to the DOM
-    document.body.appendChild(renderare.view);
+    document.body.appendChild(renderare.view); // Add rendered view to the document
 	
-	timer = setInterval(laggTillNyText, textIntervall);
+	timer = setInterval(laggTillNyText, textIntervall); // Start text spawn timer
 	
-	document.addEventListener('keypress', nyckelHanterar);
+	document.addEventListener('keypress', nyckelHanterar); // Key press handler
 	
-    requestAnimFrame(animera);
+    requestAnimFrame(animera); // Start game / animation loop
 }
 
 function animera() 
-{
-	// render the stadium   
-	renderare.render(stadium);
+{   
+	renderare.render(stadium); // Render everything
 	
-	for (index in texter)
+	for (index in texter) // Make all visible texts move to the right
 	{
 		texter[index].obj.position.x += textHastighet;
 	}
 	
-	requestAnimFrame(animera);
+	requestAnimFrame(animera); // Loop
 }
 
-function laggTillNyText()
+function laggTillNyText() // Function to spawn new texts on the screen
 {
-	var slumpvis = Math.floor(Math.random() * ord.length);
-	var slumpvisOrd = ord[slumpvis];
+	var slumpvis = Math.floor(Math.random() * ord.length); // Random text bank index
+	var slumpvisOrd = ord[slumpvis]; // Random text bank word
 	
-	var text = new PIXI.Text(slumpvisOrd);
-	text.position.x = -100;
-	var slumpvisY = Math.floor(Math.random() * 560) + 20;
+	var text = new PIXI.Text(slumpvisOrd); // New PIXI text object
+	text.position.x = -100; // Set text horizontal position
+	
+	var slumpvisY = Math.floor(Math.random() * 560) + 20; // Random text vertical position
 	text.position.y = slumpvisY;
-	stadium.addChild(text, {});
 	
-	texter.push({ obj: text, text: slumpvisOrd});
+	stadium.addChild(text, {}); // Add text to stage
+	
+	texter.push({ obj: text, text: slumpvisOrd}); // Add text to visible text array
 	
 	clearInterval(timer);
 	timer = setInterval(laggTillNyText, textIntervall);
 }
 
-function nyckelHanterar(e)
+function nyckelHanterar(e) // Key press handling function
 {
-	console.log(e.keyCode);
-	
-	for (index in texter)
+	for (index in texter) // Loop through all visible texts
 	{
-		var forst = texter[index].text.charAt(0);
-		if (forst.charCodeAt(0) == e.charCode)
+		var forst = texter[index].text.charAt(0); // Get first character of a word
+		
+		if (forst.charCodeAt(0) == e.charCode) // If the first character is the same character that the player pressed
 		{
-			texter[index].obj.setText(texter[index].text.substr(1));
-			texter[index].text = texter[index].text.substr(1);
+			texter[index].obj.setText(texter[index].text.substr(1)); // Remove first character
+			texter[index].text = texter[index].text.substr(1); // Rebuild the word without the first character
 			
-			if (texter[index].text.length <= 0)
+			if (texter[index].text.length <= 0) // If the word is gone / written
 			{
-				delete texter[index];
+				delete texter[index]; // Delete the text from visible texts
 				
-				textHastighet += 0.05;
-				textIntervall -= 50;
+				textHastighet += 0.05; // Increase text move speed
+				textIntervall -= 50; // Increase text spawn speed
 			}
 		}
 	}
